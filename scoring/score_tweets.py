0aff4a37c6156822ad48f8f4bc951fc7f53fe85d
Josh Reini <joshreini1@gmail.com>
2021-11-28 21:24:09 -0500

    tokenize tweets

--- scoring/score_tweets.py
+++ scoring/score_tweets.py
@@ -4,3 +4,15 @@ The purpose of this script is to score tweets.
 
 Author: Josh Reini (joshreini1@gmail.com)
 '''
+
+import pandas as pd
+import nltk
+from ntlk.corpus import stopwords
+
+stopwords = stopwords.words('english')
+
+twitter_data = pd.read_csv('data\\tweets\\tweets.csv')
+twitter_data['tokenized_tweet'] = twitter_data.\
+    apply(lambda row: nltk.word_tokenize(row['full_text']), axis=1)
+
+twitter_data.to_csv('data\\tweets\\token_tweets.csv')
