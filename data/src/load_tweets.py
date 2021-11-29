e034b0bb2fad701fefad11f156c3c50b313cb144
Josh Reini <joshreini1@gmail.com>
2021-11-28 21:25:19 -0500

    create dataset with tweets and write out

--- data/src/load_tweets.py
+++ data/src/load_tweets.py
@@ -13,37 +13,47 @@ import pandas as pd
 
 #get twitter api keys from dot env
 load_dotenv()
-consumer_key = os.getenv('consumer_key')
-consumer_secret = os.getenv('consumer_secret')
+api_key = os.getenv('api_key')
+api_key_secret = os.getenv('api_key_secret')
+bearer_token = os.getenv('bearer_token')
 access_token = os.getenv('access_token')
 access_token_secret = os.getenv('access_token_secret')
 
-
 #load text file containing twitter user ids
-user_ids = pd.read_csv('..\\users\\twitter_users.csv')
+user_ids = pd.read_csv('data\\users\\twitter_users.csv')
 user_ids = pd.DataFrame(user_ids)
 
 #pull tweets using tweepy
-auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth = tw.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+api = tw.API(auth, wait_on_rate_limit=True)
 parser=tw.parsers.JSONParser() #get a nice printed json response
 
-for user in len(user_ids):
-    user_id = user_ids['twitter'].values[user]
-    user = api.get_user(user_id)
+twitter_data = []
 
-    tweets = api.user_timeline(screen_name=user_id, 
-                            # 200 is the maximum allowed count
-                            count=200,
-                            include_rts = False,
-                            # Necessary to keep full_text 
-                            # otherwise only the first 140 words are extracted
-                            tweet_mode = 'extended'
-                            )
+for i in range(0,len(user_ids)):
+    user_id = user_ids['twitter'].values[i]
+
+    print(user_id)
+    try:
+        tweets = api.user_timeline(screen_name=user_id, 
+                                # 200 is the maximum allowed count
+                                count=200,
+                                include_rts = False,
+                                # Necessary to keep full_text 
+                                # otherwise only the first 140 words are extracted
+                                tweet_mode = 'extended'
+                                )
+    except Exception:
+        pass
 
     for info in tweets[:3]:
         print("ID: {}".format(info.id))
         print(info.created_at)
         print(info.full_text)
+        twitter_data.append({'user_id' : user_id, 'created_at' : info.created_at, 'full_text': info.full_text})
         print("\n")
+
+twitter_data = pd.DataFrame(twitter_data)
+
+twitter_data.to_csv('data\\tweets\\tweets.csv',index=False)
